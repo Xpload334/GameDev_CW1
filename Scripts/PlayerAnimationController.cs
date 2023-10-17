53ed8aaf520f0b56c250b842c2349f2c1d369d64
Xpload7 <js7vern@gmail.com>
2023-10-17 13:51:38 +0100

    Assets

---
+++ Scripts/PlayerAnimationController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    
+    [SerializeField] private PlayerController playerController;
+    private Animator anim;
+    private static readonly int AnimationPar = Animator.StringToHash("AnimationPar");
+    private static readonly int JumpPar = Animator.StringToHash("Jump");
+    private static readonly int GroundedPar = Animator.StringToHash("IsGrounded");
+
+    private static readonly int FunnyFlipPar = Animator.StringToHash("FunnyFlip");
+    [SerializeField] private Vector3 desiredRotation;
+    private Vector3 desiredLocalPosition = new Vector3(0, -1f, 0);
+    [SerializeField] private float rotationSpeed = 5f;
+    private float positionChangeSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // playerController = GetComponent<PlayerController>();
+        anim = GetComponent<Animator>();
+        desiredRotation = new Vector3(0, 90, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (playerController.horizontalInput)
+        {
+            //Animate walking
+            case 0:
+                anim.SetInteger (AnimationPar, 0);
+                break;
+            case > 0:
+                desiredRotation.y = 90;
+                anim.SetInteger (AnimationPar, 1);
+                break;
+            default:
+                desiredRotation.y = -90;
+                anim.SetInteger (AnimationPar, 1);
+                break;
+        }
+        
+        //Animate flipping
+        if (playerController.gravityStrength > 0)
+        {
+            desiredRotation.z = 180;
+            desiredLocalPosition.y = 1f;
+        }
+        else
+        {
+            desiredRotation.z = 0;
+            desiredLocalPosition.y = -1f;
+        }
+        
+        //Animate falling
+        anim.SetBool(GroundedPar, playerController.IsGrounded());
+
+        //Handle position lerp
+        transform.localPosition = Vector3.Lerp(transform.localPosition, desiredLocalPosition, positionChangeSpeed * Time.deltaTime);
+        //Handle rotation lerp
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(desiredRotation), rotationSpeed * Time.deltaTime);
+    }
+
+    public void TriggerJumpAnim()
+    {
+        anim.SetTrigger(JumpPar);
+    }
+
+    public void funnyFlip()
+    {
+        anim.SetTrigger(FunnyFlipPar);
+    }
+    
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private ScreenFader screenFader;
+    [SerializeField] private PlayerAnimationController playerAnimationController;
+    [SerializeField] private LayerMask groundLayerMask;
+    private Rigidbody2D rb;
+    private Collider2D collider;
+    //Actions
+    public InputAction moveAction;
+    public InputAction flipGravityAction;
+    public InputAction funnyFlipAction;
+    
+    
+    public bool grounded;
+    public float horizontalInput;
+    public float speed = 7.0f;
+    public Vector2 velocity;
+    public float gravityStrength = -10;
+
+    void Start ()
+    {
+        // playerAnimationController = GetComponent<PlayerAnimationController>();
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        screenFader = FindObjectOfType<ScreenFader>();
+
+        moveAction.Enable();
+        flipGravityAction.Enable();
+        
+        velocity = Vector3.zero;
+    }
+
+    void Update ()
+    {
+        horizontalInput = moveAction.ReadValue<float>(); //read horizontal movement
+        velocity.x = horizontalInput * speed;
+
+        grounded = IsGrounded();
+        if (flipGravityAction.triggered && IsGrounded()) //check if flip gravity triggered, and player is grounded
+        {
+            FlipGravity();
+        }
+
+        // velocity.y += gravityStrength * Time.deltaTime; //Traditional Gravity
+        // //Max velocity checks
+        // if (velocity.y > maxVelocity) velocity.y = maxVelocity;
+        // if (velocity.y < -maxVelocity) velocity.y = -maxVelocity;
+        velocity.y = gravityStrength; //No acceleration gravity, more like VVVVVV
+        rb.velocity = velocity; //move object
+
+        if (funnyFlipAction.triggered)
+        {
+            playerAnimationController.funnyFlip();
+        }
+    }
+
+    //Can only flip gravity when grounded
+    void FlipGravity()
+    {
+        gravityStrength *= -1; //reverse gravity
+        playerAnimationController.TriggerJumpAnim();
+        // velocity.y = 0; //Add small boost
+    }
+
+    public bool IsGrounded()
+    {
+        float extraHeightTest = 0.2f; //Gives you a little bit of room to remain grounded
+        var bounds = collider.bounds;
+        
+        //Get ray direction
+        Vector3 raydirection = Vector3.zero;
+        if (gravityStrength < 0)
+        {
+            raydirection.y = -1;
+        }
+        else
+        {
+            raydirection.y = 1;
+        }
+
+        //draw raycast
+        bool raycastHit = Physics2D.Raycast(bounds.center, raydirection, bounds.extents.y + extraHeightTest);
+        Color rayColour;
+        if (raycastHit)
+        {
+            rayColour = Color.green;
+        }
+        else
+        {
+            rayColour = Color.red;
+        }
+        Debug.DrawRay(bounds.center, raydirection * (bounds.extents.y + extraHeightTest));
+        return raycastHit;
+    }
+}
