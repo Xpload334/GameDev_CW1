b8c8f187f36903521bd723b66154a47c001f0983
harry <hdlockwood2003@gmail.com>
2023-10-23 16:25:26 +0100

    Preliminary and probably buggy horizontal gravity flipping

---
+++ Scripts/HorizontalObjectGravity.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalObjectGravity : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField] private LayerMask groundLayerMask; // What layer to use for ground tiles
+    public float horizontalGravity = -50f; // Strength of horizontal gravity
+    [SerializeField] private float flipBoostStrength = 5f; // Boost in velocity to give when flipping
+    [SerializeField] private float terminalVelocity = 20f; // Max velocity
+    public float playerSpeed = 7.0f;
+    [SerializeField] private float movementSmoothing = 0.05f;
+    private Vector2 m_Velocity;
+    private Vector2 _velocity;
+    public bool invert;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        if (invert) horizontalGravity = -horizontalGravity;
+
+        // Apply horizontal gravity manually
+        Vector2 horizontalGravityForce = new Vector2(horizontalGravity, 0);
+        _rb.AddForce(horizontalGravityForce);
+    }
+
+
+    public void FlipGravity()
+    {
+        // Reverse gravity direction
+        horizontalGravity = -horizontalGravity;
+        _rb.velocity = new Vector2(0,0);
+        // Apply horizontal gravity manually
+        Vector2 horizontalGravityForce = new Vector2(horizontalGravity, 0);
+        _rb.AddForce(horizontalGravityForce);
+    }
+
+    void FixedUpdate()
+    {
+    }
+}
