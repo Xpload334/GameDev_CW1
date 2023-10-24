c554dceeaa2cad6d98bf0a7000a78299252dcfb7
Xpload7 <js7vern@gmail.com>
2023-10-24 11:09:44 +0100

    Added delay option for moving objects; added grey tile to tileset for prototyping; began work on Level 4

--- Scripts/PatrolPoints.cs
+++ Scripts/PatrolPoints.cs
@@ -11,8 +11,10 @@ public class PatrolPoints : MonoBehaviour
 
     [SerializeField] private Vector3 objectivePoint;
 
+    public bool ShouldPatrol = true;
     public bool ShouldMove = true;
     public float speed = 5f;
+    public float waitAtEachPoint = 1f;
     [SerializeField] private float thresholdDistance = 0.01f;
     // Start is called before the first frame update
     void Start()
@@ -31,27 +33,14 @@ public class PatrolPoints : MonoBehaviour
         
         objectivePointIndex = NextPointIndex(); //Set objective to next point
         objectivePoint = patrolPoints[objectivePointIndex];
+        
+        
+        StartCoroutine(PatrolAroundPoints());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If reached objective point
-        if (Vector2.Distance(transform.position, objectivePoint) < thresholdDistance)
-        {
-            //Switch to next objective point
-            objectivePointIndex = NextPointIndex();
-            objectivePoint = patrolPoints[objectivePointIndex]; 
-        }
-        
-        
-        //Move towards target point
-        if (ShouldMove)
-        {
-            var step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, objectivePoint, step);
-        }
-        
         
     }
 
@@ -64,5 +53,33 @@ public class PatrolPoints : MonoBehaviour
         }
 
         return i;
+    }
+
+    IEnumerator PatrolAroundPoints()
+    {
+        while (ShouldPatrol)
+        {
+            //If reached objective point
+            if (Vector2.Distance(transform.position, objectivePoint) < thresholdDistance)
+            {
+                //Switch to next objective point
+                objectivePointIndex = NextPointIndex();
+                objectivePoint = patrolPoints[objectivePointIndex];
+                
+                //Wait for a bit
+                yield return new WaitForSeconds(waitAtEachPoint);
+            }
+            
+            
+            //Move towards target point
+            if (ShouldMove)
+            {
+                var step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, objectivePoint, step);
+            }
+
+            yield return null; //Wait a frame
+        }
+        
     }
 }
